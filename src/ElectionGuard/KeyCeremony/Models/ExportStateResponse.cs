290ad5f4bf37f2b401f3b3b8abc970a1bcb5e705
Keith Fung <keith.robert.fung@gmail.com>
2019-10-09 14:45:32 -0400

    🏗 Establish Election Guard SDK Project and Solution
    Start initial C Sharp project for Election Guard SDK
    - Start with Key Ceremony Trustee example to showcase the intended path for the code. 
    - Setup testing framework
    - Setup Projects and solution file

---
+++ src/ElectionGuard/KeyCeremony/Models/ExportStateResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ElectionGuard.SDK.KeyCeremony.Models
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ExportStateResponse
+    {
+        public TrusteeStatus status;
+        public TrusteeState state;
+    }
+}
---
+++ src/ElectionGuard/KeyCeremony/Models/GenerateKeyResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ElectionGuard.SDK.KeyCeremony.Models
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GenerateKeyResponse
+    {
+        public TrusteeStatus Status;
+        public KeyGeneratedMessage Message;
+    }
+}
