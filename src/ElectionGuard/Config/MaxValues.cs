2c25b9713ac5379d3a88f648357e5ae74dc3b08b
Keith Fung <keith.robert.fung@gmail.com>
2019-10-17 16:13:43 -0400

    🚧 Update Key Ceremony and Constants
    Replicate constants in C to ensure same limitations are imposed on C# equivalent

---
+++ src/ElectionGuard/Config/MaxValues.cs
@@ -0,0 +1,9 @@
+﻿namespace ElectionGuard.SDK.Config
+{
+    public static class MaxValues
+    {
+        public static readonly uint MaxTrustees = 5;
+        public static readonly uint MaxBallots = 1000;
+        public static readonly uint MaxSelections = 1000;
+    }
+}
