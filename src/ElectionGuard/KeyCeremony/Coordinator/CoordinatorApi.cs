504991ccf888b70fc72d06e26fdce4cc8d425349
Keith Fung <keith.robert.fung@gmail.com>
2019-10-17 16:13:43 -0400

    🚧 Update Key Ceremony and Constants
    Replicate constants in C to ensure same limitations are imposed on C# equivalent

--- src/ElectionGuard/KeyCeremony/Coordinator/CoordinatorApi.cs
+++ src/ElectionGuard/KeyCeremony/Coordinator/CoordinatorApi.cs
@@ -11,24 +11,24 @@ namespace ElectionGuard.SDK.KeyCeremony.Coordinator
         internal static extern NewCoordinatorReturn NewCoordinator(uint numberOfTrustees, uint threshold);
 
         [DllImport("electionguard", EntryPoint = "KeyCeremony_Coordinator_free")]
-        internal static extern void FreeCoordinator(UIntPtr c);
+        internal static extern void FreeCoordinator(UIntPtr coordinator);
 
         [DllImport("electionguard", EntryPoint = "KeyCeremony_Coordinator_receive_key_generated")]
-        internal static extern CoordinatorStatus ReceiveKeyGenerated(UIntPtr c, KeyGeneratedMessage message);
+        internal static extern CoordinatorStatus ReceiveKeyGenerated(UIntPtr coordinator, KeyGeneratedMessage message);
 
         [DllImport("electionguard", EntryPoint = "KeyCeremony_Coordinator_all_keys_received")]
-        internal static extern AllKeysReceivedReturn AllKeysReceived(UIntPtr c);
+        internal static extern AllKeysReceivedReturn AllKeysReceived(UIntPtr coordinator);
 
         [DllImport("electionguard", EntryPoint = "KeyCeremony_Coordinator_receive_shares_generated")]
-        internal static extern CoordinatorStatus ReceiveSharesGenerated(UIntPtr c, SharesGeneratedMessage message);
+        internal static extern CoordinatorStatus ReceiveSharesGenerated(UIntPtr coordinator, SharesGeneratedMessage message);
 
         [DllImport("electionguard", EntryPoint = "KeyCeremony_Coordinator_all_shares_received")]
-        internal static extern AllSharesReceivedReturn AllSharesReceived(UIntPtr c);
+        internal static extern AllSharesReceivedReturn AllSharesReceived(UIntPtr coordinator);
 
         [DllImport("electionguard", EntryPoint = "KeyCeremony_Coordinator_receive_shares_verified")]
-        internal static extern CoordinatorStatus ReceiveSharesVerified(UIntPtr c, SharesVerifiedMessage message);
+        internal static extern CoordinatorStatus ReceiveSharesVerified(UIntPtr coordinator, SharesVerifiedMessage message);
 
         [DllImport("electionguard", EntryPoint = "KeyCeremony_Coordinator_publish_joint_key")]
-        internal static extern PublishJointKeyReturn PublishJointKey(UIntPtr c);
+        internal static extern PublishJointKeyReturn PublishJointKey(UIntPtr coordinator);
     }
 }
