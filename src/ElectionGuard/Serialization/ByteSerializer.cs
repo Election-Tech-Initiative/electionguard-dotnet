ebc46bc8a338142a887793616b1e8b24f7ffb2fa
Keith Fung <keith.robert.fung@gmail.com>
2019-11-11 16:03:35 -0500

    🚧 Create Serialization for Public Key and Trustee State
    Create serializers that can be used for manipulating the public key and private key aka trustee state

---
+++ src/ElectionGuard/Serialization/ByteSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ElectionGuard.SDK.Serialization
+{
+    public static class ByteSerializer
+    {
+        private const int FromBase = 16;
+
+        public static byte[] SerializeFromRaw(string raw)
+        {
+            var rawArray = raw.Split('-');
+            var array = new byte[rawArray.Length];
+            for (var i = 0; i < rawArray.Length; i++) array[i] = Convert.ToByte(rawArray[i], FromBase);
+            return array;
+        }
+
+        public static byte[] SerializeFromBase64(string base64)
+        {
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
