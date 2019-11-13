43420b6a586bc2e46994c8b43e9c310089c775b9
Pamela Vong <pamelavong@gmail.com>
2019-11-13 18:21:17 -0500

    sed may not be available in githubs mac/check grep

--- scripts/download-latest.sh
+++ scripts/download-latest.sh
@@ -6,7 +6,7 @@ FILE="electionguard.zip"
 RELEASES="https://api.github.com/repos/$REPO/releases"
 
 echo "Determining latest release"
-TAG=$(curl --silent "$RELEASES" | grep -m1 '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+TAG=$(curl --silent "$RELEASES" | grep -m1 '"tag_name":')
 echo "Latest release version found: $TAG"
 
 DOWNLOAD="https://github.com/$REPO/releases/download/$TAG/$FILE"
