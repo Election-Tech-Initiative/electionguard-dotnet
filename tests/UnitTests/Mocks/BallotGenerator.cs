cd235819b6393f414c5b8390a9401980c32a0a08
Pamela Vong <pamelavong@gmail.com>
2019-12-03 15:57:48 -0500

    C# Wrapper side that implements the C API expected number of selected

--- tests/UnitTests/Mocks/BallotGenerator.cs
+++ tests/UnitTests/Mocks/BallotGenerator.cs
@@ -1,38 +1,33 @@
 ﻿using ElectionGuard.SDK;
 using System;
+using System.Linq;
 
 namespace UnitTests.Mocks
 {
     public static class BallotGenerator
     {
-        public static bool[] FillRandomBallot(int numberOfSelections)
+        public static bool[] FillRandomBallot(int numberOfSelections, int expectedNumberOfSelected)
         {
             if (numberOfSelections > Constants.MaxSelections)
             {
                 throw new ArgumentOutOfRangeException(nameof(numberOfSelections));
             }
 
+            // parallel array to use as the look up to set the random index of other array to true
+            // if numberOfSelections is 5, this constructs an array of [0, 1, 2, 3, 4]
+            var sourceIndexes = Enumerable.Range(0, numberOfSelections).ToArray();
+
+            var random = new Random();
             var selections = new bool[numberOfSelections];
-            var selected = false;
-            for (uint i = 0; i < numberOfSelections; i++)
+            for (uint i = 0; i < expectedNumberOfSelected; i++)
             {
-                if (!selected)
-                {
-                    selections[i] = RandomBit();
-                }
-                else
-                {
-                    selections[i] = false;
-                }
-                if (selections[i])
-                {
-                    selected = true;
-                }
+                var randomSourceIndex = random.Next(sourceIndexes.Length);
+                var indexOfSelections = sourceIndexes[randomSourceIndex];
+                selections[indexOfSelections] = true;
+                // remove the indexOfSelections from the sourceIndexes array
+                sourceIndexes = sourceIndexes.Where(val => val != indexOfSelections).ToArray();
             }
-            if (!selected)
-            {
-                selections[numberOfSelections - 1] = true;
-            }
+
             return selections;
         }
 
