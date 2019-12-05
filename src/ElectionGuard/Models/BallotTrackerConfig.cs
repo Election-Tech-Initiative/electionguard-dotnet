2e8e448139e93cf04474fcbf05bc489f190ad745
Keith Fung <keith.robert.fung@gmail.com>
2019-12-05 12:27:04 -0500

    🔥 Delete Voting Works Models
    Voting works models should be at a higher level instead of existing at low level since they are implementation specific

--- src/ElectionGuard/Models/BallotTrackerConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ElectionGuard.SDK.Models
-{
-    public class BallotTrackerConfig
-    {
-        public string TrackerType { get; set; }
-        public string TrackerSiteDisplay { get; set; }
-        public string TrackerUrlTemplate { get; set; }
-    }
-}
--- src/ElectionGuard/Models/District.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ElectionGuard.SDK.Models
-{
-    public class District
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
-}
